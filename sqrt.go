d0531c1637b06fb1c0371bc47ad7db84eed0e306
tu.le <lethanhtu401@gmail.com>
2025-04-19 11:07:41 +0700

    add-solution

---
+++ sqrt.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(mySqrt(8))
+	fmt.Println(mySqrt(16))
+}
+
+func mySqrt(x int) int {
+	if x == 1 {
+		return 1
+	}
+
+	for i := 1; i < x/2; i++ {
+		if i*i == x {
+			return i
+		}
+
+		if i*i > x {
+			return i - 1
+		}
+	}
+
+	return 0
+}
